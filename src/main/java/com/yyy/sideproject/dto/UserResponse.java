2301bc31b5cf6ebcc6cadd4fe822bed7f087274c
Yubin <seoyubin12@gmail.com>
2026-05-01 18:45:02 +0900

    새로운 기능 구현 완료

--- src/main/java/com/yyy/sideproject/dto/UserResponse.java
+++ src/main/java/com/yyy/sideproject/dto/UserResponse.java
@@ -10,4 +10,6 @@ public class UserResponse {
     private Long id;
     private String name;
     private String email;
+    private String password;
+    private String role;
 }
--- src/main/java/com/yyy/sideproject/repository/UserRepository.java
+++ src/main/java/com/yyy/sideproject/repository/UserRepository.java
@@ -1,7 +1,17 @@
 package com.yyy.sideproject.repository;
 
 import com.yyy.sideproject.domain.User;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+	// 1. ID 내림차순(최신순)으로 정렬해서 가져오기
+    //List<User> findAllByOrderByIdDesc();
+
+    // 2. 이름 오름차순(가나다순)으로 정렬해서 가져오기
+    List<User> findAllByOrderByIdAsc();
+    
+    // 3. 생성일(regDate) 내림차순으로 정렬해서 가져오기
+    //List<User> findAllByOrderByRegDateDesc();
 }
