2b45ab3728592e8dcadccd51683b336c70d2efa6
DESKTOP-C1T83CR\USER <ajy4606@naver.com>
2026-06-07 19:17:34 +0900

    팀원 코드 병합 전 내 작업 저장

--- src/main/java/com/yyy/sideproject/controller/FaqController.java
+++ src/main/java/com/yyy/sideproject/controller/FaqController.java
@@ -1,7 +1,5 @@
 package com.yyy.sideproject.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
